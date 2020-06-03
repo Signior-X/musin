7b9fc2129b3f9ef7518e378f5f9003ab57694c10
Signior-X <b19188@students.iitmandi.ac.in>
2020-06-03 15:57:20 +0530

    Add top navigation at home content

--- app/src/main/java/com/example/musin/Main2Activity.java
+++ app/src/main/java/com/example/musin/Main2Activity.java
@@ -1,8 +1,11 @@
 package com.example.musin;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Menu;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.google.android.material.navigation.NavigationView;
 
